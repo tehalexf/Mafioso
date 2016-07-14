67cd95c75df91e0861d78b600358ca9b3169d74e
GWCUser <GWCUser>
2016-07-14 15:42:52 -0700

    Added more code

--- client/main.js
+++ client/main.js
@@ -1,22 +1,166 @@
-// import '../imports/ui/body.js';
 
-Router.route('/', {
-  name: 'home'
-});
+//Generates the access code
+function generateAccessCode(){
+    var code = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz";
 
-Router.route('/posts', {
-  controller: 'PostController',
-  action: 'index'
-});
+      for(var i=0; i < 6; i++){
+        code += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
 
-Router.route('/posts/:_id', {
-  controller: 'PostController',
-  action: 'show'
-});
+      return code;
+}
+  
+generateNewGame = function (){
+  var game = {
+    accessCode: generateAccessCode(),
+    state: "waitingForPlayers",
+    location: null,
+    lengthInMinutes: 8,
+    endTime: null,
+    paused: false,
+    pausedTime: null
+  };
+
+  var gameID = Games.insert(game);
+  game = Games.findOne(gameID);
+  console.log(game)
+  return game;
+}
+
+generateNewPlayer = function(game, name){
+  var player = {
+    gameID: game._id,
+    name: name,
+    role: null,
+    // isSpy: false,
+    // isFirstPlayer: false
+  };
+
+  var playerID = Players.insert(player);
+
+  return Players.findOne(playerID);
+}
 
 if (Meteor.isClient) {
+
+  WebFontConfig = {
+    google: { families: [ 'Gloria+Hallelujah::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+
+  Template.startMenu.events({
+    'click #btn-join-game': function(e){ 
+      $("#joinroom").removeClass("hidden");
+      console.log($("#roomid").val())
+
+
+      Meteor.call( 'insertTaco', 0, 1, 2, 3, ( error ) => {
+      if ( error ) {
+        console.log( error );
+      }
+      });
+
+
+      // $("#btn-join-game").addClass("hidden");
+      // $("#btn-new-game").addClass("hidden");
+      
+       
+
+
+    }, 
+
+    'click #btn-new-game': function(event){ 
+      playerName = $("#playerName").val()
+      if (!playerName)
+          return false;
+
+      // var playerName = event.target.playerName.value;
+      // console.log(playerName)
+      console.log(event)
+
+      var game = generateNewGame();
+      var player = generateNewPlayer(game, playerName);
+
+      Meteor.subscribe('games', game.accessCode);
+      // $("#btn-join-game").addClass("hidden");
+      // $("#btn-new-game").addClass("hidden");
+       Session.set("loading", true);
+       Meteor.subscribe('players', game._id, function onReady(){
+      Session.set("loading", false);
+
+      Session.set("gameID", game._id);
+      Session.set("playerID", player._id);
+      Session.set("currentView", "lobby");
+    });
+
+
+    }
+
+
+     // 'click #btn-new-game': function (event) {
+     //  var playerName = event.target.playerName.value;
+     //  console.log(playerName)
+     // };
+
+
+
+  });
+
+
+
+// Template.startMenu.events({
+//   'submit #create-game': function (event) {
+//     GAnalytics.event("game-actions", "newgame");
+
+    
+
+//     if (!playerName || Session.get('loading')) {
+//       return false;
+//     }
+
+//     var game = generateNewGame();
+//     var player = generateNewPlayer(game, playerName);
+
+//     Meteor.subscribe('games', game.accessCode);
+
+//     Session.set("loading", true);
+    
+//     Meteor.subscribe('players', game._id, function onReady(){
+//       Session.set("loading", false);
+
+//       Session.set("gameID", game._id);
+//       Session.set("playerID", player._id);
+//       Session.set("currentView", "lobby");
+//     });
+
+//     return false;
+//   },
+//   'click .btn-back': function () {
+//     Session.set("currentView", "startMenu");
+//     return false;
+//   }
+// });
+
+
+  Template.main.helpers({
+    whichView: function() {
+      return Session.get('currentView');
+    },
+
+  });
+
+
   ApplicationController = RouteController.extend({
     layoutTemplate: 'AppLayout',
+
 
     onBeforeAction: function () {
       console.log('app before hook!');
@@ -30,7 +174,7 @@ if (Meteor.isClient) {
 
   HomeController = ApplicationController.extend({
     action: function () {
-      this.render('Home');
+      this.render('Home1');
     }
   });
 
--- server/main.js
+++ server/main.js
@@ -1,1 +1,26 @@
-import '../imports/api/tasks.js';
+Meteor.methods({
+  insertTaco( count ) {
+    gameTemp.insert( { count: count } );
+  }
+
+
+});
+
+if (Meteor.isServer) {
+
+  Meteor.startup(function() {
+
+    return Meteor.methods({
+
+      removeAllPosts: function() {
+Games.remove({});
+Players.remove({});
+        return gameTemp.remove({});
+
+      }
+
+    });
+
+  });
+
+}
